88878a689bdd74825299ec2737768ef7325579f3
Virajitha Dilshan <virajitha.Jayathilaka@gmail.com>
2026-05-14 17:16:05 +0000

    backend done v1

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const jobRoutes = require("./routes/jobRoutes");
+const errorHandler = require("./middleware/errorHandler");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("GlobalTNA Job Request API is running");
+});
+
+app.use("/api/jobs", jobRoutes);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
